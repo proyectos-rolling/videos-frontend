279810e5376fc76efb9e5bfb786e4d931a475ede
Benjamin Lavena <benja@zmaster.com.ar>
2020-12-19 23:44:55 -0300

    Creo un método para obtener el thumbnail de un video y lo muestro en el listado

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import Menu from './navbar';
 import Pie from './footer';
 
+const getVideoImageUrl = (url) => {
+  const preRegex = /.*v=(.*)/; //capturo todo lo que está después del "v="
+  let videoId = url.replace(preRegex, "$1"); //creo una string que tenga todo lo capturado
+  const posRegex = /(.*?)(&.*|$)/; //campturo todo lo q está antes de un & o toda la string
+  videoId = videoId.replace(posRegex, "$1"); //reemplazo con lo capturado
+  const VideoImageUrl = ` https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`; //de este modo obtengo la thumbnail del video
+  return VideoImageUrl
+}
+
 
 function App() {
   const apiurl = process.env.REACT_APP_APIURL;
@@ -31,6 +40,7 @@ function App() {
             <ul>
               {category.videos.map((video) => (
                 <li>
+                  <img src={getVideoImageUrl(video.url)} alt={video.title} />
                   <a href={video.url}>{video.title}</a>
                 </li>
               ))}
